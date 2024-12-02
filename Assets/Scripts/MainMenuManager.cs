1d2a1d80a748a2d81f7151b7c136d5d01a764086
root <root@ChristiansCPU>
2024-12-02 07:18:12 -0500

    Main menu mostly done. Quit button needs fixing, slight title screen changes.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement; // For scene management
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Called when the mouse hovers over the button
+    public void OnHoverEnter(GameObject button)
+    {
+        // Example: Change the button color on hover
+        var buttonImage = button.GetComponent<UnityEngine.UI.Image>();
+        if (buttonImage != null)
+        {
+            buttonImage.color = Color.yellow; // Change to yellow on hover
+        }
+    }
+
+    // Called when the mouse stops hovering over the button
+    public void OnHoverExit(GameObject button)
+    {
+        // Reset the button color when not hovered
+        var buttonImage = button.GetComponent<UnityEngine.UI.Image>();
+        if (buttonImage != null)
+        {
+            buttonImage.color = Color.white; // Reset to white
+        }
+    }
+
+    // Play Button: Loads the CharacterSelect scene
+    public void OnPlayButtonClick()
+    {
+        SceneManager.LoadScene("CharacterSelect");
+    }
+
+    // Settings Button: Loads the Settings scene
+    public void OnSettingsButtonClick()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    // Quit Button: Exits the game
+    public void OnQuitButtonClick()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting..."); // Debug log for testing in the editor
+    }
+}
+
